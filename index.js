40af0d3a4d0a858ce808829103260c73f648779f
Jeremy Brown <jeremy@notmessenger.com>
2016-10-24 02:18:47 -0500

    Refactored application logic

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const chalk = require('chalk')
-const log = console.log
+const format = require('./lib/format')
+const game = require('./lib/game')
 const input = require('./lib/input')
-const validation = require('./lib/validation.js')
+const log = console.log
+const validation = require('./lib/validation')
 
 try {
-  validation.validateCards(input[2])
+  const cards = input[2]
+
+  validation.validateCards(cards)
+
+  log(
+    chalk.green(
+      game.identifyHand(format(cards))
+    )
+  )
 } catch (err) {
-  log(chalk.red(err.message))
+  log(
+    chalk.red(
+      err.message
+    )
+  )
 }

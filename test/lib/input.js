977bceeb66b415af0f1ab02c50e6bd4fa65745c2
Jeremy Brown <jeremy@notmessenger.com>
2016-10-24 00:46:00 -0500

    Test for input library

---
+++ test/lib/input.js
@@ -0,0 +1,16 @@
+const assert = require('assert')
+const input = require('../../lib/input')
+
+describe('getInput()', function () {
+  it('Returns expected value', function () {
+    const testValue = 'test Value 3'
+
+    process.argv[2] = testValue
+
+    assert.equal(
+      input[2],
+      testValue,
+      'Gets process arguments'
+    )
+  })
+})

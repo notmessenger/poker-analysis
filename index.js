b2400285a80227a8461f086dea86f8bb5695f3ac
Jeremy Brown <jeremy@notmessenger.com>
2016-10-24 03:14:14 -0500

    Display provided hand, replacing suit information with icons

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const format = require('./lib/format')
 const game = require('./lib/game')
 const input = require('./lib/input')
 const log = console.log
+const output = require('./lib/output')
 const validation = require('./lib/validation')
 
 try {
   const cards = input[2]
+  const formattedCards = format(cards)
 
   validation.validateCards(cards)
 
   log(
     chalk.green(
-      game.identifyHand(format(cards))
+      'The hand of',
+      output(formattedCards),
+      'is',
+      game.identifyHand(formattedCards)
     )
   )
 } catch (err) {

488dd4187c5002eb5364b593e01b51ffb605180f
Jeremy Brown <jeremy@notmessenger.com>
2016-10-24 01:22:15 -0500

    Change organization of tests

--- test/lib/input.js
+++ test/lib/input.js
@@ -1,16 +1,18 @@
 const assert = require('assert')
 const input = require('../../lib/input')
 
-describe('getInput()', function () {
-  it('Returns expected value', function () {
-    const testValue = 'test Value 3'
+describe('Input Data', function () {
+  describe('getInput()', function () {
+    it('Returns expected value', function () {
+      const testValue = 'test Value 3'
 
-    process.argv[2] = testValue
+      process.argv[2] = testValue
 
-    assert.equal(
-      input[2],
-      testValue,
-      'Gets process arguments'
-    )
+      assert.equal(
+        input[2],
+        testValue,
+        'Gets process arguments'
+      )
+    })
   })
 })
